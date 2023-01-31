2052d9d0a7a7bcde8ccf5024fde3917aac7ae703
mcsymiv <s.mcsymiv@gmail.com>
2023-01-31 14:20:26 +0200

    [be] add render tmpl, [fe] add base page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,4 +4,7 @@ import "net/http"
 
 func (a *app) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
 	a.infoLog.Println("hit virtual terminal")
+	if err := a.renderTemplate(w, r, "terminal", nil); err != nil {
+		a.errorLog.Println("unable to render terminal page template", err)
+	}
 }
--- cmd/web/render.go
+++ cmd/web/render.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"html/template"
+	"net/http"
 )
 
-type templateDate struct {
+// templateData holds default data structures
+// that can be passed to template pages
+type templateData struct {
 	StringMap       map[string]string
 	IntMap          map[string]int
 	FloatMap        map[string]float32
@@ -19,7 +23,100 @@ type templateDate struct {
 	CSSVersion      string
 }
 
+// functions holds custom template functions
+// that can be passed to template pages
 var functions = template.FuncMap{}
 
-// go:embed templates
+// Go comment directive
+// embeds template directory to binaries
+// syntax: //directive (no space between `//` and directive)
+//
+//go:embed templates
 var templateFileSystem embed.FS
+
+// default template directory name
+// contains .partial.tmpl, .base.tmpl, page.tmpl files
+var tempDirectory string = "templates"
+
+// add DefaultTemplateData populates templateData struct
+// with default data structures and/or values in tempalate pages
+func (a *app) addDefaultTemplateData(td *templateData, r *http.Request) *templateData {
+	return td
+}
+
+func (a *app) renderTemplate(w http.ResponseWriter, r *http.Request, page string, td *templateData, partials ...string) error {
+	var t *template.Template
+	var err error
+
+	// template to render from templates directory
+	toRender := fmt.Sprintf("%s/%s.page.tmpl", tempDirectory, page)
+
+	// checks if app already has template to render
+	// in cached templateCache map
+	// if no template is cached
+	// renders new template
+	_, cached := a.templageCache[toRender]
+	if !cached {
+		a.infoLog.Println(fmt.Sprintf("parsing %s template", toRender))
+		t, err = a.parseTemplate(toRender, page, partials)
+		if err != nil {
+			a.errorLog.Println("unable to parse template")
+
+			return err
+		}
+	}
+
+	// Gets template from application config cache
+	// if page has already been parsed and created
+	t = a.templageCache[toRender]
+
+	// Add default template data if none was provided
+	// to the page template
+	if td == nil {
+		td = &templateData{}
+	}
+
+	// Add default template data to provided in render
+	td = a.addDefaultTemplateData(td, r)
+
+	// Execute template
+	err = t.Execute(w, td)
+	if err != nil {
+		a.errorLog.Println("unable to execute template")
+
+		return err
+	}
+
+	return nil
+}
+
+// parseTemplate builds pages from template partials
+// (base, defined pages, layouts)
+func (a *app) parseTemplate(tmplToTender, page string, partials []string) (*template.Template, error) {
+	var t *template.Template
+	var err error
+
+	for i, p := range partials {
+		partials[i] = fmt.Sprintf("%s/%s.partial.tmpl", tempDirectory, p)
+	}
+
+	t, err = template.
+		New(fmt.Sprintf("%s.page.tmpl", page)).
+		Funcs(functions).
+		ParseFS(
+			templateFileSystem,
+			fmt.Sprintf("%s/base.layout.tmpl", tempDirectory),
+			// strings.Join(partials, ","),
+			tmplToTender,
+		)
+	if err != nil {
+		a.errorLog.Println("unable to create new template", err)
+
+		return nil, err
+	}
+
+	// Puts parsed template to application config cache
+	a.templageCache[tmplToTender] = t
+
+	return t, nil
+}
